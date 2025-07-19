38777aa7aabca164724ad37769693e1cb1c11741
mohammed raad <mraad6689@gmail.com>
2025-07-19 14:53:34 +0300

    change formating

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,4 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import { pgTableCreator } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
